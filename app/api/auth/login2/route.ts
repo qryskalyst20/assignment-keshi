545478ea7e52d3d2c9812bd3c220044478e13613
nrmnqdds <quddusnuriman@gmail.com>
2024-01-08 08:31:08 +0800

    :sparkles:

--- app/api/auth/login2/route.ts
+++ app/api/auth/login2/route.ts
@@ -1,11 +1,24 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import bcrypt from "bcryptjs";
+import { cookies } from "next/headers";
 
 export async function POST(request: NextRequest) {
   const { email, password } = await request.json();
 
   if (email === "admin@gmail.com") {
     if (password === "password123") {
+      const hashedToken = await bcrypt.hash(email, 10);
+
+      cookies().set({
+        name: "sessionToken",
+        value: hashedToken as string,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        path: "/",
+        maxAge: 60 * 60 * 24 * 14,
+      });
       return NextResponse.json({ success: true, user: { email } });
     }
   }
